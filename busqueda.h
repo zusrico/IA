fc23da041e3e2948fd07c9e3cdfcac5a37381509
zusrico <jesusgarciaflt@gmail.com>
2022-10-28 01:52:17 +0200

    estado P3

---
+++ busqueda.h
@@ -0,0 +1,27 @@
+/*******************************************/
+/* 		    BUSQUEDA.H                     */
+/*						                   */
+/* Asignatura: Inteligencia Artificial     */
+/* Grado en Ingenieria Informatica - UCA   */
+/*******************************************/
+#include "puzzlito.h"
+
+#ifndef _tNodo_
+#define _tNodo_
+typedef struct NodoBusqueda_{
+    tEstado *estado;
+    unsigned operador;
+    int costeCamino;
+    int profundidad;
+    int valHeuristica;
+    struct NodoBusqueda_ *padre;
+} tNodo;
+
+#endif
+
+int busquedaAnchura(void);
+int busquedaAnchuraControlEstados(void);
+int busquedaProfundidad(void);
+int busquedaProfundidadControlEstados(void);
+int EstadosRepetidos(LISTA, tNodo*);
+void solucionFin(int res);
---
+++ main.c
@@ -0,0 +1,39 @@
+/*******************************************/
+/*             MAIN.C                      */
+/*                                         */
+/* Asignatura: Inteligencia Artificial     */
+/* Grado en Ingenieria Informatica - UCA   */
+/*******************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+//#include "puzzlito.h"
+#include "listaia.h"
+#include "busqueda.h"
+
+int main(){
+    int busca;
+    printf("1.Anchura.\n2.Profundidad.\n3.Anchura Control de Estados Repetidos.\n4.Profundidad Control de Estados Repetidos.\n 5.Salir\nIntroduce el metodo de busqueda deseado:");
+    scanf("%d", &busca);
+
+    switch (busca)
+    {
+    case 1:
+        solucionFin(busquedaAnchura());
+        break;
+    case 2:
+        solucionFin(busquedaProfundidad());
+    case 3:
+        solucionFin(busquedaAnchuraControlEstados());
+        break;
+    case 4:
+        solucionFin(busquedaProfundidadControlEstados());
+        break;
+    default:
+        break;
+    }
+
+    return 0;
+}
