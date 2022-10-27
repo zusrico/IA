00892a80db602cc592007e1fce1372e14a9da424
zusrico <jesusgarciaflt@gmail.com>
2022-10-27 15:17:43 +0200

    Estado practica 2

---
+++ busqueda.h
@@ -0,0 +1,24 @@
+/*******************************************/
+/* 		    BUSQUEDA.H                     */
+/*						                   */
+/* Asignatura: Inteligencia Artificial     */
+/* Grado en Ingenieria Informatica - UCA   */
+/*******************************************/
+#include "puzzlito.h"
+
+#ifndef _tNodo_
+#define _tNodo_
+typedef struct NodoBusqueda_{
+    tEstado *estado;
+    unsigned operador;
+    int costeCamino;
+    int profundidad;
+    int valHeuristica;
+    struct NodoBusqueda_ *padre;
+} tNodo;
+
+#endif
+
+int busquedaAnchura(void);
+int busquedaProfundidad(void);
+void solucionFin(int res);
